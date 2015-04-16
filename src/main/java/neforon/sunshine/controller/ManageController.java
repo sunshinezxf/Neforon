756b7878eaf474e50a45398e24df3952cdd5cfef
sunshine <zxfan11@software.nju.edu.cn>
2015-04-16 21:22:15 +0800

    add manager index page initial

--- src/main/java/neforon/sunshine/controller/ManageController.java
+++ src/main/java/neforon/sunshine/controller/ManageController.java
@@ -4,6 +4,7 @@ import neforon.sunshine.manager.facade.ManagerFacade;
 import neforon.sunshine.utils.ResponseCode;
 import neforon.sunshine.utils.ResultData;
 import neforon.sunshine.utils.URLConst;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,11 +27,17 @@ public class ManageController {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        ResultData authenticationMessage = managerFacade.queryManager(username, password);
-        if (authenticationMessage.getStatusCode() == ResponseCode.MESSAGE_OK) {
-            
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            view.setViewName("error");
+            return view;
         }
 
+        ResultData authenticationMessage = managerFacade.queryManager(username, password);
+        if (authenticationMessage.getStatusCode() == ResponseCode.MESSAGE_OK) {
+            view.setViewName("manage");
+        } else {
+            view.setViewName("error");
+        }
         return view;
     }
 }
